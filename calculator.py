dc554f812a2e2f97c8a51148ed9fd4daba4aa94b
37sean <118905725+37sean@users.noreply.github.com>
2022-11-23 17:23:49 +0000

    Add unit tests for calculator

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 Calculator library containing basic maths functions
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
+
 
 def subtract(first_term, second_term):
     return first_term - second_term
